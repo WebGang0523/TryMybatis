880749047b68c440c4f3efc9958683fed02c2ce3
xuchenggang <chenggang.xu@xipu.com>
2022-05-03 09:11:48 +0800

    init

---
+++ src/com/xucg/mybatis/bean/Employee.java
@@ -0,0 +1,50 @@
+package com.xucg.mybatis.bean;
+
+public class Employee {
+    private Integer id;
+    private String lastName;
+    private String email;
+    private String gender;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
---
+++ src/com/xucg/mybatis/dao/EmployeeMapper.java
@@ -0,0 +1,9 @@
+package com.xucg.mybatis.dao;
+
+import com.xucg.mybatis.bean.Employee;
+
+public interface EmployeeMapper {
+	
+	public Employee getEmpById(Integer id);
+
+}
